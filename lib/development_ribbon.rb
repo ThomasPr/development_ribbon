3fb0cc3f9ea1c4b35447ded2176a82a5e8648c74
Thomas Preißler <thomas.preissler@gmx.de>
2012-09-22 00:10:38 +0200

    test helper

--- lib/development_ribbon.rb
+++ lib/development_ribbon.rb
@@ -1,5 +1,5 @@
 require "development_ribbon/version"
-require "development_ribbon/engine"
+require "development_ribbon/engine" if defined? Rails
 
 module DevelopmentRibbon
   # Your code goes here...
---
+++ spec/spec_helper.rb
@@ -0,0 +1,1 @@
+require 'development_ribbon'
